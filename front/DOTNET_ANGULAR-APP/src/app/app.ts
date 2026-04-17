0ccff6ca7f7b2b6d2583fb193252f6a198a22f82
Alexandre Rangel <agrangeljunior1315@gmail.com>
2026-04-17 16:53:18 -0300

    linkei o front com o banco de dados

--- front/DOTNET_ANGULAR-APP/src/app/app.ts
+++ front/DOTNET_ANGULAR-APP/src/app/app.ts
@@ -2,12 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Palestrantes } from './palestrantes/palestrantes';
 import { EventosComponent } from "./eventos/eventos";
-import { HttpClient} from '@angular/common/http';
+import { HttpClient, HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Palestrantes, EventosComponent],
+  imports: [RouterOutlet, Palestrantes, EventosComponent,HttpClientModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- front/DOTNET_ANGULAR-APP/src/app/eventos/eventos.ts
+++ front/DOTNET_ANGULAR-APP/src/app/eventos/eventos.ts
@@ -1,27 +1,34 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core'; // Importe o ChangeDetectorRef
 
 @Component({
   selector: 'app-eventos',
+  standalone: true, // Se estiver usando Angular 17+
   imports: [],
   templateUrl: './eventos.html',
   styleUrl: './eventos.scss',
 })
 export class EventosComponent implements OnInit {
   
-  public eventos: any;
+  public eventos: any = []; // Inicialize como array vazio para evitar erros no template
 
-  constructor(private http: HttpClient) { }
+  // Injete o ChangeDetectorRef no construtor
+  constructor(
+    private http: HttpClient,
+    private cdr: ChangeDetectorRef 
+  ) { }
 
   ngOnInit(): void {
     this.getEventos();
   }
 
   public getEventos(): void {
-    this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
-    );
+    this.http.get('https://localhost:5001/api/evento').subscribe({
+      next: (response) => {
+        this.eventos = response;
+        this.cdr.detectChanges(); // Força a verificação de mudanças após receber os dados
+      },
+      error: (error) => console.log(error)
+    });
   }
 }
