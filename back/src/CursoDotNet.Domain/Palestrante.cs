3ca4f937ffacf39f365e7220fb38a92c4f52a653
Alexandre Rangel <agrangeljunior1315@gmail.com>
2026-05-13 11:13:43 -0300

    fixing stringinfo to string

--- back/src/CursoDotNet.Domain/Palestrante.cs
+++ back/src/CursoDotNet.Domain/Palestrante.cs
@@ -8,8 +8,8 @@ namespace CursoDotNet.Domain
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-        public StringInfo MiniCurriculo { get; set; }
-        public StringInfo ImagemURL     { get; set; }
+        public string MiniCurriculo { get; set; }
+        public string ImagemURL     { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
         public IEnumerable<RedeSocial> RedesSociais { get; set; }
