29f58a7cc9f219609f2bc9ccfb984946d46cf7b7
Alexandre Rangel <agrangeljunior1315@gmail.com>
2026-05-28 18:05:37 -0300

    ajustando layout do filtro e do botão de criar evento

--- front/DOTNET_ANGULAR-APP/src/app/shared/titulo/titulo.component.ts
+++ front/DOTNET_ANGULAR-APP/src/app/shared/titulo/titulo.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, input, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-titulo',
@@ -9,6 +9,7 @@ export class TituloComponent implements OnInit {
   @Input() titulo: string = '';
   @Input() subtitulo: string = 'Desde 2021';
   @Input() iconClass: string = 'fa fa-user';
+  @Input() botaoListar: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
